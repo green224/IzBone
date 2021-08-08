e28412f533fec80efa7d3e786c733eded03e39de
green224 <green224@users.noreply.github.com>
2021-08-08 14:38:06 +0900

    refac: 不要なコードを削除

--- unity/Assets/IzBone/Common/Runtime/Field/HalfLife.cs
+++ unity/Assets/IzBone/Common/Runtime/Field/HalfLife.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using Unity.Mathematics;
 using static Unity.Mathematics.math;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 
 namespace IzBone.Common.Field {
 
--- unity/Assets/IzBone/Common/Runtime/Field/PowRange.cs
+++ unity/Assets/IzBone/Common/Runtime/Field/PowRange.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using Unity.Mathematics;
 using static Unity.Mathematics.math;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 
 namespace IzBone.Common.Field {
 
@@ -43,14 +39,9 @@ namespace IzBone.Common.Field {
 		// 表示する値の位置(0～1)と、実際の値(left～right)との相互変換
 		public float srcValue2showValue(float srcValue) =>
 			(float)srcValue2showValue(srcValue, baseNum, left, right, isShowPowValue);
-//			return isShowPowValue
-//				? (float)linValue2powValue_withLinLR( srcValue, baseNum, left, right )
-//				: (float)powValue2linValue_withPowLR( srcValue, baseNum, left, right );
 		public float showValue2srcValue(float showValue) =>
 			(float)showValue2srcValue(showValue, baseNum, left, right, isShowPowValue);
-//			return isShowPowValue
-//				? (float)powValue2linValue_withLinLR( showValue, baseNum, left, right )
-//				: (float)linValue2powValue_withPowLR( showValue, baseNum, left, right );
+
 
 		// 表示する値の位置(0～1)と、実際の値(left～right)との相互変換をstaticで行う処理
 		static public double srcValue2showValue(
@@ -91,59 +82,6 @@ namespace IzBone.Common.Field {
 				: linValue2powValue(a, baseNum);
 		}
 
-//		// 対数変化の値(0～1)と、線形変化の値(0～1)との相互変換
-//		static public float linearValue2powValue(float linearValue, float baseNum) =>
-//			(float)linearValue2powValue((double)linearValue, baseNum);
-//		static public float powValue2linearValue(float poweredValue, float baseNum) =>
-//			(float)powValue2linearValue((double)poweredValue, baseNum);
-//		static public double linearValue2powValue(double realValue, double baseNum) {
-//			var a = clamp(realValue, 0, 1);
-//
-//			a = ( pow(baseNum, a) - 1 ) / ( baseNum - 1 );
-//
-//			return clamp(a, 0, 1);
-//		}
-//		static public double powValue2linearValue(double poweredValue, double baseNum) {
-//			var a = clamp(poweredValue, 0, 1);
-//
-//			a = log2(a*(baseNum-1) + 1) / log2(baseNum);
-//
-//			return clamp(a, 0, 1);
-//		}
-
-//		// 対数変化の値(0～1)と、線形変化の値(0～1)との相互変換。
-//		// 計算精度を保つために、Left/Rightをどちら側で持つかによって式を変えている。
-//		static public double linValue2powValue_withPowLR(
-//			double linearValue, double baseNum,
-//			double poweredLeft, double poweredRight
-//		) {
-//			var a = pow(baseNum, linearValue);
-//			a = ( a - poweredLeft ) / ( poweredRight - poweredLeft );
-//			return clamp(a, 0, 1);
-//		}
-//		static public double linValue2powValue_withLinLR(
-//			double linearValue, double baseNum,
-//			double linearLeft, double linearRight
-//		) => linValue2powValue_withPowLR(
-//			linearValue, baseNum,
-//			pow(baseNum, linearLeft), pow(baseNum, linearRight)
-//		);
-//		static public double powValue2linValue_withLinLR(
-//			double poweredValue, double baseNum,
-//			double linearLeft, double linearRight
-//		) {
-//			var a = log2(poweredValue) / log2(baseNum);
-//			a = ( a - linearLeft ) / ( linearRight - linearLeft );
-//			return clamp(a, 0, 1);
-//		}
-//		static public double powValue2linValue_withPowLR(
-//			double poweredValue, double baseNum,
-//			double poweredLeft, double poweredRight
-//		) => powValue2linValue_withLinLR(
-//			poweredValue, baseNum,
-//			log2(poweredLeft)  / log2(baseNum),
-//			log2(poweredRight) / log2(baseNum)
-//		);
 
 		// 対数変化の値(0～∞)と、線形変化の値(-∞～∞)との相互変換。
 		static public double linValue2powValue(double linearValue, double baseNum) =>
